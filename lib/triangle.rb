3758c00c06ff17cf6682c7c3c64a8caea798ea0e
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-29 21:16:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
   # write code here
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
