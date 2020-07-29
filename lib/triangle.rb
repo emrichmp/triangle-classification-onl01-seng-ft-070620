73c60bea15fee0af93fa4c1dadf5c5629623a478
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-29 21:23:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,23 @@ class Triangle
     @third = third
   end
   
+  def kind()
+    if @first <= 0 || @second <= 0 || @third <= 0
+      raise TriangleError
+    elsif @first+@second <= @third || @first+@third <= @second || @second+@third <= @first
+      raise TriangleError
+    end
+    if @first == @second && @second == @third
+      :equilateral
+    elsif @first == @second || @second == @third || @first == @third
+      :isosceles
+    elsif @first != @second && @second != @third && @first != @third
+      :scalene
+    end
+  end
+  
+end
+
   class TriangleError < StandardError
     
   end
-end
